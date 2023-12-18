af3f71e03f9e8c7e8d76a605585da4e2246f90b8
Bhesh Lama <bheshlama195@gmail.com>
2023-12-18 12:17:13 +0545

    s

--- resources/js/components/pages/pramukhKaryaharu/PramukhKaryaharuSubpage.jsx
+++ resources/js/components/pages/pramukhKaryaharu/PramukhKaryaharuSubpage.jsx
@@ -2,10 +2,14 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import StartingBanner from "../../forAll/StartingBanner";
 import { useParams } from "react-router-dom";
+import book1 from "../../../images/img.webp";
+import book2 from "../../../images/img2.webp";
+import myPdf from "../../../assets/myBook.pdf";
+
 import { lazy } from "react";
 const LinksSection = lazy(() => import("../../forAll/LinksSection"));
 
-const PramukhKaryaharuSubpage = () => {
+const PramukhKaryaharuSubpage = ({ books }) => {
     const { subLinkId } = useParams();
     const [majorWorksDetailApi, setMajorWorksDetailApi] = useState();
     const fetchMajorWorksDetail = async () => {
@@ -23,7 +27,7 @@ const PramukhKaryaharuSubpage = () => {
     return (
         <>
             <StartingBanner />
-            <div className="parichaya-subpage">
+            <div className="pramukhKaryaharu-subpage">
                 <div className="contents">
                     <div className="main-section">
                         <h1 className="heading">
@@ -31,14 +35,44 @@ const PramukhKaryaharuSubpage = () => {
                                 majorWorksDetailApi.title &&
                                 majorWorksDetailApi.title}
                         </h1>
-                        <span
-                            dangerouslySetInnerHTML={{
-                                __html:
-                                    majorWorksDetailApi &&
-                                    majorWorksDetailApi.description &&
-                                    majorWorksDetailApi.description,
-                            }}
-                        />
+                        {Number(subLinkId) === 1 ? (
+                            <span
+                                dangerouslySetInnerHTML={{
+                                    __html:
+                                        majorWorksDetailApi &&
+                                        majorWorksDetailApi.description &&
+                                        majorWorksDetailApi.description,
+                                }}
+                            />
+                        ) : (
+                            <div className="books">
+                                {books.map((book) => {
+                                    const {
+                                        id = "",
+                                        name = "",
+                                        image_link = "",
+                                        pdf_link = "",
+                                    } = book;
+                                    return (
+                                        <a
+                                            className="book-card"
+                                            key={id}
+                                            href={pdf_link}
+                                            target="__blank"
+                                        >
+                                            <img
+                                                className="book-img"
+                                                src={image_link}
+                                                alt=""
+                                            />
+                                            <div className="book-name">
+                                                {name}
+                                            </div>
+                                        </a>
+                                    );
+                                })}
+                            </div>
+                        )}
                     </div>
                     <LinksSection />
                 </div>
@@ -48,3 +82,68 @@ const PramukhKaryaharuSubpage = () => {
 };
 
 export default PramukhKaryaharuSubpage;
+
+PramukhKaryaharuSubpage.defaultProps = {
+    books: [
+        {
+            id: 1,
+            name: "The Love Hypothesis",
+            image_link: book1,
+            pdf_link: myPdf,
+        },
+        {
+            id: 2,
+            name: "How to Talk to anyone",
+            image_link: book2,
+            pdf_link: myPdf,
+        },
+        {
+            id: 3,
+            name: "The Love Hypothesis",
+            image_link: book1,
+            pdf_link: myPdf,
+        },
+        {
+            id: 4,
+            name: "How to Talk to anyone",
+            image_link: book2,
+            pdf_link: myPdf,
+        },
+        {
+            id: 5,
+            name: "The Love Hypothesis",
+            image_link: book1,
+            pdf_link: myPdf,
+        },
+        {
+            id: 6,
+            name: "How to Talk to anyone",
+            image_link: book2,
+            pdf_link: myPdf,
+        },
+        {
+            id: 7,
+            name: "The Love Hypothesis",
+            image_link: book1,
+            pdf_link: myPdf,
+        },
+        {
+            id: 8,
+            name: "How to Talk to anyone",
+            image_link: book2,
+            pdf_link: myPdf,
+        },
+        {
+            id: 9,
+            name: "The Love Hypothesis",
+            image_link: book1,
+            pdf_link: myPdf,
+        },
+        {
+            id: 10,
+            name: "How to Talk to anyone",
+            image_link: book2,
+            pdf_link: myPdf,
+        },
+    ],
+};
